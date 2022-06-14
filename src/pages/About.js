c4a2706e7585615e25d729402be73dd2046bbbb2
Iyana Marquez <yayakix@gmail.com>
2022-06-14 19:42:51 -0400

    editng

--- src/pages/About.js
+++ src/pages/About.js
@@ -3,19 +3,19 @@ import { useState, useEffect } from "react";
 
 function About(props) {
   const [about, setAbout] = useState([]);
-  const getAboutData = async () =>{
+  const getAboutData = async () => {
     const response = await fetch(props.URL + 'about');
     const data = await response.json();
-    // console.log(data)
     setAbout(data);
   }
+
   useEffect(() => getAboutData, []);
 
   const loaded = () => (
     <div>
       <h2>{about?.name}</h2>
       <h2>{about?.email}</h2>
-      <img src={about?.headshot} />
+      <img src={about?.headshot} alt='about'/>
       <p>{about?.bio} </p>
     </div>
   );
--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -5,23 +5,25 @@ function Projects(props) {
   const getProjectData = async () => {
     const response = await fetch(props.URL + "projects");
     const data = await response.json();
-    console.log(data);
     setProject(data);
+
   };
+
   useEffect(() => getProjectData, []);
 
   const loaded = () => {
-    return projects.map((project) => (
+    return projects.map((project, index) => (
+      
       <div>
         <h1>{project.name}</h1>
-        <img src={project.image}/>
-        <p>{project.live}</p>
-        <p>{project.git}</p>
+        <img src={project.image} alt='project' />
+        <br></br>
+        <a href={project.live}>live site</a>
+        <br></br>
+        <a href={project.git}>github link</a>
       </div>
     ));
-
   }
-   
 
   return projects ? loaded() : <h1>Loading...</h1>;
 }
