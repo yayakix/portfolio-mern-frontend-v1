6e50d1afc7343406577aef33fbd9e0ab1ec4ba11
Iyana Marquez <yayakix@gmail.com>
2022-06-15 22:12:08 -0400

    icons to personal sites

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,5 +1,14 @@
 function Home() {
-  return <div >home</div>;
+  return (
+    <div>
+      <a href="https://github.com/yayakix">
+        <i class="fa-brands fa-github"></i>
+      </a>
+      <a href="https://www.linkedin.com/in/iyana-marquez/">
+        <i class="fa-brands fa-linkedin"></i>
+      </a>
+    </div>
+  );
 }
 
 export default Home;
